27303121a9bdbe262107a8a19d6e17511fa8242e
Gav Hensley <gavinrhensley@gmail.com>
2021-04-06 11:32:11 -0400

    added properties and constructors

--- backend/Studio-Inventory-API/Studio-Inventory-API/Models/Category.cs
+++ backend/Studio-Inventory-API/Studio-Inventory-API/Models/Category.cs
@@ -7,5 +7,19 @@ namespace Studio_Inventory_API.Models
 {
     public class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Equipment> EquipmentList { get; set; }
+
+        public Category()
+        {
+
+        }
+
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
